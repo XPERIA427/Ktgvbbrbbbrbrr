6f7535af6e4f9aa66551f903c8ac9519ebc50645
XPERIA427 <anggorodeni921@gmail.com>
2026-02-01 22:20:42 +0700

    Create quick_start.sh

---
+++ quick_start.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo "[⚡] Installing TikTok Follower Termux Brutal..."
+cd ~
+git clone https://github.com/your-repo/tiktok-termux-brutal.git
+cd tiktok-termux-brutal
+chmod +x install.sh
+./install.sh
